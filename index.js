a22943492b4a277f10c4196f5a3752b140085842
Clare-Boniface <clarelgarfield@hotmail.com>
2023-08-11 17:02:56 +0100

    update the JS so the images loop

--- index.js
+++ index.js
@@ -16,27 +16,57 @@ const currentDog = new Dog(dogs[currentDogIndex]);
 renderDog(currentDog);
 
 function reject() {
-  console.log("click reject");
   document.getElementById("nope-sticker").classList.remove("hidden");
   document.getElementById("like-sticker").classList.add("hidden");
+  delay();
+  //   getNewDog();
 }
 
 function like() {
-  console.log("click like");
+  //   console.log("click like");
   document.getElementById("like-sticker").classList.remove("hidden");
   document.getElementById("nope-sticker").classList.add("hidden");
+  delay();
+  //   getNewDog();
 }
 
 function renderDog(currentDog) {
+  //   delay();
   postContainerEl.innerHTML = currentDog.getDogHtml();
+  //   document.getElementById("like-sticker").classList.add("hidden");
+  //   document.getElementById("nope-sticker").classList.add("hidden");
+}
+
+// function match() {
+//   currentDog.getMatchStatus(true);
+//   getNewDog(currentDog);
+
+function delay() {
+  setTimeout(getNewDog, 2000);
 }
 
 // function getNewDog() {
-//   setTimeout(function () {
-//     currentDogIndex += 1;
-//     currentDog = new Dog(dogs[currentDogIndex]);
-//     renderDog();
-//   }, 2000);
+//   document.getElementById("like-sticker").classList.add("hidden");
+//   document.getElementById("nope-sticker").classList.add("hidden");
+//   currentDogIndex += 1;
+//   if (dogs.length === currentDogIndex + 1) {
+//     renderDog(currentDog);
+//   } else {
+//     currentDogIndex = 0;
+//     renderDog(currentDog);
+//   }
+function getNewDog() {
+  if (currentDogIndex + 1 >= dogs.length) {
+    currentDogIndex = 0;
+  } else {
+    currentDogIndex += 1;
+  }
+  const newDog = new Dog(dogs[currentDogIndex]);
+  renderDog(newDog);
+
+  //   setTimeout(getNewDog, 2000);
+}
+
 // }
 
 // function match() {
@@ -45,4 +75,4 @@ function renderDog(currentDog) {
 // }
 // function setBackground(path) {
 //   postContainerEl.style.backgroundImage = `url(${path})`;
-// }
+//
